e7d5c1d886444d8c3c3162e2fc3826fcd5d5f2e1
Israel Abebe <se.israel.abebe@gmail.com>
2022-06-23 11:17:00 +0300

    Time: 102 ms, Memory: 14.9 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.py
+++ duplicate-zeros/duplicate-zeros.py
@@ -8,9 +8,27 @@ class Solution:
         
         while i < len_array:
             if arr[i] == 0:
-                arr[:] = arr[:i+1]+arr[i:]
-                arr[:] = arr[:len_array]
+                arr.insert(i,0)
+                arr.pop()
                 i += 1
             i += 1
+        
+        
+        
+        
+#         FIRST ATTEMPT
+#         Runtime: 1087 ms
+#         Memory Usage: 14.9 MB  
+
+
+#         len_array = len(arr)    
+#         i = 0
+        
+#         while i < len_array:
+#             if arr[i] == 0:
+#                 arr[:] = arr[:i+1]+arr[i:]
+#                 arr[:] = arr[:len_array]
+#                 i += 1
+#             i += 1
         
         
