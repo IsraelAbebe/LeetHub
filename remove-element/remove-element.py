68884e9362ea6c4b4748d065fbbeb53d677d7e65
Israel Abebe <se.israel.abebe@gmail.com>
2022-06-25 20:50:19 +0300

    Time: 38 ms, Memory: 13.8 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -1,9 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # DUMMY WAY SINCE I CANT DO THE EFFICENT ONE
         new_list = []
         for index, value in enumerate(nums):
             if value != val:
-                print(value,val)
                 new_list.append(value)
         
         nums[:] = new_list
