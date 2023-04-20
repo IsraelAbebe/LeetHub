02311b4fb6b2fce8045d05629821605f6407389d
Israel Abebe <se.israel.abebe@gmail.com>
2023-04-20 11:27:13 +0200

    Time: 30 ms, Memory: 13.8 MB - LeetHub

--- remove-element/remove-element.py
+++ remove-element/remove-element.py
@@ -1,13 +1,20 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        if len(nums) < 1:
+            return len(nums)-1
         
-        custom_index = 0
-        for index, value in enumerate(nums):
-            if value != val:
-                nums[custom_index] = value
-                custom_index += 1
+        if len(nums) == 1:
+            return len(nums) if nums[0] != val else 0
                 
-        return custom_index
+            
+        
+        left = [i for i in nums if i != val ]
+        right = [i for i in nums if i == val]
+        
+        nums[:] = left+right
+        # print(nums, left,right,len(left))
+        return len(left)
+        
                 
             
                 
