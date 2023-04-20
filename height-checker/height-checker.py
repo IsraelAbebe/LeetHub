f88f7f503015b24340eaad3ca11aa832b436391e
Israel Abebe <se.israel.abebe@gmail.com>
2023-04-20 12:08:55 +0200

    Time: 52 ms, Memory: 13.9 MB - LeetHub

---
+++ height-checker/height-checker.py
@@ -0,0 +1,19 @@
+import random
+class Solution:
+    def sort(self,heights: List[int]) -> List[int]:
+        if len(heights) <= 1:
+            return heights
+        
+        pivote = random.choice(heights)
+        
+        left = [i for i in heights if i < pivote]
+        middle = [i for i in heights if i == pivote]
+        right = [i for i in heights if i > pivote]
+        
+        return self.sort(left)+middle+self.sort(right)
+    
+    
+    def heightChecker(self, heights: List[int]) -> int:
+        print([i==j for i,j in zip(self.sort(heights),heights)])
+        return [i==j for i,j in zip(self.sort(heights),heights)].count(False)
+    
