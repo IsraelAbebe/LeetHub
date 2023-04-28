0e38ec404efb149b9e778411048fe78f53ed0345
Israel Abebe <se.israel.abebe@gmail.com>
2023-04-28 10:41:42 +0200

    Time: 572 ms, Memory: 18.1 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,33 +1,24 @@
+import random
+
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        returnList = []
-        leftPointer, rightPointer = 0,len(nums)-1
+        if len(nums) == 0:
+            return []
+        elif len(nums) == 1:
+            nums.insert(0,nums.pop()**2)
+            return nums
         
-        while True:
-            if leftPointer == rightPointer:
-                returnList.insert(0,nums[leftPointer] ** 2)
-                break
+        first_index,last_index = 0,len(nums)-1
+        
+        while first_index <= last_index:
+            if nums[first_index]**2 >= nums[last_index]**2:
+                nums.insert(last_index,nums.pop(first_index)**2)
+                last_index -= 1
+            elif nums[first_index]**2 < nums[last_index]**2:
+                nums.insert(last_index,nums.pop(last_index)**2)
+                last_index -= 1
                 
-            if nums[leftPointer] ** 2 <= nums[rightPointer] ** 2:
-                returnList.insert(0,nums[rightPointer] ** 2)
-                rightPointer -= 1
-            else:
-                returnList.insert(0,nums[leftPointer] ** 2)
-                leftPointer += 1
-            
-        # while leftPointer <= rightPointer:
-        #     if nums[leftPointer] ** 2 <= nums[rightPointer] ** 2:
-        #         returnList.insert(0,nums[rightPointer] ** 2)
-        #         rightPointer -= 1
-        #     else:
-        #         returnList.insert(0,nums[leftPointer] ** 2)
-        #         leftPointer += 1
-            
-            
-            
-        
-        return returnList
-            
+        return nums
             
         
         
